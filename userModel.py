288c93fb7ea6c9b230932fb0d01e064061984db2
kevinnat123 <knathanael09@gmail.com>
2025-07-02 10:27:24 +0700

    [ Model ] User

--- userModel.py
+++ userModel.py
@@ -3,4 +3,9 @@ from flask_login import UserMixin
 class User(UserMixin):
     def __init__(self, username, user_data):
         self.id = username
+        self.nama = user_data["nama"]
+        self.role = user_data["role"]
         self.user_data = user_data
+
+    def get_id(self):
+        return self.id
