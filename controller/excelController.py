7e3c5d8e6a72cb133a763df931bb7b482ffd7568
kevinnat123 <knathanael09@gmail.com>
2025-07-14 14:07:46 +0700

    [ Export ] Ambil beban sks dari ga

--- controller/excelController.py
+++ controller/excelController.py
@@ -86,9 +86,7 @@ def export_jadwal_to_excel(jadwal_list, matakuliah_list, dosen_list):
     # ===== ===== ===== ===== ===== ===== ===== ===== ===== =====
     worksheet = workbook.add_worksheet("BEBAN SKS DOSEN")
     object_jadwal = [JadwalKuliah(**dict_jadwal) for dict_jadwal in jadwal_list]
-    beban_dosen = {}
-    for dosen in dosen_list:
-        beban_dosen[dosen["nip"]] = ga.hitung_beban_sks_dosen(jadwal=object_jadwal, nip=dosen["nip"])
+    beban_dosen = ga.hitung_beban_sks_dosen(jadwal=object_jadwal, dosen_list=dosen_list)
     # Tulis data beban dosen
     worksheet.merge_range("A1:C1", "Dosen Tetap", format_title)
     worksheet.merge_range("E1:G1", "Dosen Tidak Tetap", format_title)
